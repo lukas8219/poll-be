09b6db92b3b1bdcfce42ded4df63108c125bc2c1
lucas <lucas.c4d@gmail.com>
2021-12-07 22:50:42 -0300

    finished

--- security/src/main/java/com/lukas8219/pollbe/data/dto/UserPhotoDTO.java
+++ security/src/main/java/com/lukas8219/pollbe/data/dto/UserPhotoDTO.java
@@ -1,5 +1,6 @@
 package com.lukas8219.pollbe.data.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 public class UserPhotoDTO {
 
+    @JsonProperty("pic")
     private String link;
 
 }
--- security/src/main/java/com/lukas8219/pollbe/data/mapper/UserMapper.java
+++ security/src/main/java/com/lukas8219/pollbe/data/mapper/UserMapper.java
@@ -5,14 +5,17 @@ import com.lukas8219.pollbe.data.domain.User;
 import com.lukas8219.pollbe.data.domain.UserPhoto;
 import com.lukas8219.pollbe.data.dto.UserDTO;
 import com.lukas8219.pollbe.data.dto.UserPhotoDTO;
-import org.mapstruct.DecoratedWith;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 @DecoratedWith(UserDecorator.class)
 public interface UserMapper {
 
+    @Mappings({
+            @Mapping(target = "email", source = "email"),
+            @Mapping(target = "phoneNumber", source = "phoneNumber"),
+            @Mapping(target = "name", source = "name"),
+    })
     UserDTO toDTO(User source);
 
     UserPhotoDTO toPhotoDTO(UserPhoto photo);
