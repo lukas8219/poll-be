1eefb7abcad6ffa12ab9da6006eb54bbc59fd02c
Lucas Polesello <lucas.c4d@gmail.com>
2021-12-24 15:32:39 -0300

    readjusted test

--- src/main/java/com/lukas8219/pollbe/service/authentication/token/TokenServiceImpl.java
+++ src/main/java/com/lukas8219/pollbe/service/authentication/token/TokenServiceImpl.java
@@ -2,6 +2,7 @@ package com.lukas8219.pollbe.service.authentication.token;
 
 import com.lukas8219.pollbe.data.domain.PollUserDetails;
 import com.lukas8219.pollbe.data.dto.TokenDTO;
+import com.lukas8219.pollbe.data.mapper.UserMapper;
 import com.lukas8219.pollbe.security.JwtUtil;
 import com.lukas8219.pollbe.service.user.UserGetService;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +14,13 @@ public class TokenServiceImpl implements TokenService {
 
     private final JwtUtil jwtUtil;
     private final UserGetService userGetService;
+    private final UserMapper userMapper;
 
     @Override
     public TokenDTO generateToken(PollUserDetails userDetails) {
         var token = jwtUtil.generateToken(userDetails);
-        var user = userGetService.getUser(userDetails);
-        return new TokenDTO(user, token);
+        var user = userGetService.getUser(userDetails.getId());
+        return new TokenDTO(userMapper.toDTO(user), token);
     }
 
     @Override
