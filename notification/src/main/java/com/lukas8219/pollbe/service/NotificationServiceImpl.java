f5f5ad447184ac25efe0f065755cb07e1b842e2b
Lucas Polesello <lucas.c4d@gmail.com>
2022-01-10 21:10:45 -0300

    removed schedule producer

--- notification/src/main/java/com/lukas8219/pollbe/service/NotificationServiceImpl.java
+++ notification/src/main/java/com/lukas8219/pollbe/service/NotificationServiceImpl.java
@@ -1,18 +1,16 @@
 package com.lukas8219.pollbe.service;
 
-import com.lukas8219.pollbe.data.dto.NotificationType;
 import com.lukas8219.pollbe.data.dto.SendEmailDTO;
 import com.lukas8219.pollbe.factory.SendEmailStrategyFactory;
 import com.lukas8219.pollbe.producer.NotificationProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class NotificationServiceImpl implements NotificationService{
+public class NotificationServiceImpl implements NotificationService {
 
     private final SendEmailStrategyFactory strategyFactory;
     private final NotificationProducer<SendEmailDTO> emailDTONotificationProducer;
@@ -23,8 +21,4 @@ public class NotificationServiceImpl implements NotificationService{
         log.info("Strategy is {}", strategy);
     }
 
-    @Scheduled(fixedDelay = 500)
-    public void produce(){
-        emailDTONotificationProducer.publish(new SendEmailDTO(NotificationType.EMAIL, ""));
-    }
 }
