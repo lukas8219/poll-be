82b8bf25fcdf12b0f30faace13973532c2ebd8dc
Lucas Polesello <lucas.c4d@gmail.com>
2022-01-11 23:45:48 -0300

    overloaded other methods

--- security/src/main/java/com/lukas8219/pollbe/service/UserCreateServiceImpl.java
+++ security/src/main/java/com/lukas8219/pollbe/service/UserCreateServiceImpl.java
@@ -7,7 +7,6 @@ import com.lukas8219.pollbe.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -19,25 +18,30 @@ import static com.lukas8219.pollbe.data.domain.AuthenticationProviderEnum.GOOGLE
 public class UserCreateServiceImpl {
 
     private final UserRepository repository;
-    private PasswordEncoder encoder = new BCryptPasswordEncoder();
     private final String TEMPORARY_PASS = "1234";
+    private PasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    public User createOrFindByGoogle(PollOAuth2User oAuth2User){
-        return  createOrFind(oAuth2User, GOOGLE);
+    public User createOrFindByGoogle(PollOAuth2User oAuth2User) {
+        return createOrFind(oAuth2User, GOOGLE);
     }
 
     public User createOrFind(PollOAuth2User oAuth2User, AuthenticationProviderEnum provider) {
         var user = repository.findByEmailAndProvider(oAuth2User.getEmail(), provider);
         if (user.isEmpty()) {
-            var newUser = new User();
-            newUser.setEmail(oAuth2User.getEmail());
-            newUser.setName(oAuth2User.getName());
-            newUser.setPassword(encoder.encode(TEMPORARY_PASS));
-            newUser.setProvider(provider);
-            newUser.setCreatedAt(LocalDateTime.now());
-            return repository.save(newUser);
+            return create(oAuth2User.getEmail(), oAuth2User.getName(), null, provider);
         } else {
             return user.get();
         }
+    }
+
+    public User create(String email, String name, String phoneNumber, AuthenticationProviderEnum provider) {
+        var newUser = new User();
+        newUser.setEmail(email);
+        newUser.setName(name);
+        newUser.setPhoneNumber(phoneNumber);
+        newUser.setPassword(encoder.encode(TEMPORARY_PASS));
+        newUser.setProvider(provider);
+        newUser.setCreatedAt(LocalDateTime.now());
+        return repository.save(newUser);
     }
 }
